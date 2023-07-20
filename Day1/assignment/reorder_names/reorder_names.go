acd0d4110baf6019e127d70f151d5a9e77827f8c
TienAnNguyen <le3akmailg@gmail.com>
2023-07-20 20:10:00 +0700

    finish assignment

--- Day1/assignment/reorder_names/reorder_names.go
+++ Day1/assignment/reorder_names/reorder_names.go
@@ -1,6 +1,35 @@
-package reorder_names
+package main
 
 import(
-	fmt
-	os
+	"fmt"
+	"os"
 )
+
+func main(){
+
+	args := os.Args;
+	name := map[int]string{}
+
+	for i:= 1; i < len(args) - 1; i++{
+		name[i] = args[i]
+	}
+
+	rules := map[string][]int {
+
+		"US": {1, 2, 3},
+		"VN": {2, 3, 1},
+	}
+
+	order, existed := rules[ args[len(args) - 1] ]
+	if !existed {
+		order = rules["US"]
+	}
+
+	for _,o := range order {
+
+		if value, existed := name[o]; existed{
+			fmt.Print(value + " ")
+		}
+	}
+	fmt.Println()
+}
