f63c27a978300d2ac879c516d46fa22fcbab2cf2
TienAnNguyen <le3akmailg@gmail.com>
2023-07-21 10:36:44 +0700

    update code

--- Day1/assignment/reorder_names/reorder_names.go
+++ Day1/assignment/reorder_names/reorder_names.go
@@ -1,34 +1,27 @@
 package main
 
-import(
+import (
 	"fmt"
 	"os"
 )
 
-func main(){
+func main() {
+	args := os.Args
+	name := args[1 : len(args)-1]
 
-	args := os.Args;
-	name := map[int]string{}
-
-	for i:= 1; i < len(args) - 1; i++{
-		name[i] = args[i]
+	rules := map[string][]int{
+		"US": {0, 1, 2},
+		"VN": {1, 2, 0},
 	}
 
-	rules := map[string][]int {
-
-		"US": {1, 2, 3},
-		"VN": {2, 3, 1},
-	}
-
-	order, existed := rules[ args[len(args) - 1] ]
+	order, existed := rules[args[len(args)-1]]
 	if !existed {
 		order = rules["US"]
 	}
 
-	for _,o := range order {
-
-		if value, existed := name[o]; existed{
-			fmt.Print(value + " ")
+	for _, o := range order {
+		if o < len(name) {
+			fmt.Print(name[o] + " ")
 		}
 	}
 	fmt.Println()
